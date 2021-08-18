1e8ae919f5435253fc5f3ad489a7ad548de85d5c
carlosavellar <carlos.avellar@gmail.com>
2021-08-18 17:11:35 -0300

    React create context

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import "./index.css";
-import App from "./playground/use-reducer/src/App";
+import App from "./playground/react-context/src/App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
